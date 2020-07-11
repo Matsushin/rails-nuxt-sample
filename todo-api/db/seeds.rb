5bab0dfc3f50aa58c8f7a3aec4d7c61f5b42d4c1
Matsushin <matsu1983@gmail.com>
2020-07-11 11:00:58 +0900

    seedを作成

--- todo-api/db/seeds.rb
+++ todo-api/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Task.all.count.zero?
+  Task.transaction do
+    5.times do |index|
+      Task.create(title: "タスクタイトル#{index + 1}", body: 'ここにタスクの詳細内容が入ります。')
+    end
+  end
+end
